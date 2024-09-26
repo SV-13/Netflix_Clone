b461026352cb4f7eb34ff331c054265f2ff5f835
SV-13 <sujalveer13102003@gmail.com>
2024-09-27 04:50:28 +0530

    update

--- netflix_site/settings.py
+++ netflix_site/settings.py
@@ -140,7 +140,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-CSRF_TRUSTED_ORIGINS = []
+CSRF_TRUSTED_ORIGINS = ["https://netflix-clone-delta-topaz.vercel.app/"]
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
